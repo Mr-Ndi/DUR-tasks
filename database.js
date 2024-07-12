00e430696d61aa1c660ff6fac06307880478ae97
Mr-Ndi <onlythenotes@gmail.com>
2024-07-12 20:46:06 +0200

    sampling for authors database api.

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
-import mysql from "mysql2"
-import dotenv from "dotenv"
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
+
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
@@ -9,9 +10,63 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise();
 
+//  ----------------------------- Authors Endpoint ---------------------------------------------
+export async function all_authors() {
+    try {
+        const [ans] = await pool.query('select first_name, last_name from Authors');
+        return [ans];
+    } catch (err) {
+        console.error('Error fetching all authors:', err);
+        throw err;
+    }
+}
 
+export async function Single_author(id) {
+    try {
+        const [ans] = await pool.query('SELECT * FROM Authors WHERE author_id = ?', [id]);
+        return [ans];
+    } catch (err) {
+        console.error('Error fetching single author:', err);
+        throw err;
+    }
+}
 
-export async function allBooks() {
-    const [ans] = await pool.query('select * from Books')
-    return [ans]
+export async function Create_author(id, fname, lname, dob) {
+    if (!id || !fname || !lname || !dob) {
+        throw new Error('Invalid input parameters');
+    }
+
+    try {
+        const [ans] = await pool.query(
+            'INSERT INTO Authors (author_id, first_name, last_name, date_of_birth) VALUES (?, ?, ?, ?)',
+            [id, fname, lname, dob]
+        );
+        return [ans];
+    } catch (err) {
+        console.error('Error creating author:', err);
+        throw err;
+    }
+}
+
+export async function Update_author(id, fname, lname, dob) {
+    try {
+        const [ans] = await pool.query(
+            'UPDATE Authors SET first_name = ?, last_name = ?, date_of_birth = ? WHERE author_id = ?',
+            [fname, lname, dob, id]
+        );
+        return [ans];
+    } catch (err) {
+        console.error('Error updating author:', err);
+        throw err;
+    }
+}
+
+export async function Deleting_author(id) {
+    try {
+        const [ans] = await pool.query('DELETE FROM Authors WHERE author_id = ?', [id]);
+        return [ans];
+    } catch (err) {
+        console.error('Error deleting author:', err);
+        throw err;
+    }
 }
--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-import { allBooks } from "./database.js";
+import { all_authors, Single_author, Create_author, Deleting_author } from "./database.js";
+import { bodyParse } from "body-parser";
 import express2 from "express"
 
 const route = express2();
+route.use(bodyParse.json())
 
-route.get('/', (que, ans) => {
-    ans.send("This will be all books")
-})
+route.get('/author', async(que, ans) => {
+    const answer = await all_authors();
+    
+    ans.send(answer)
+    console.log("This will be all authors")
+});
 
+route.get('/author/id', async(que, ans) => {
+    const id = que.params(id)
+    const answer = await Single_author(id)
+
+    ans.send(answer)
+    console.log("This will be single author")
+});
+
+route.put('/author', (que, ans) => {
+    const {id,fname,lname,dob} = que.body;
+    const answer = Create_author(id,fname,lname,dob)
+
+    ans.send(answer)
+    console.log("This will be adding a single author")
+});
+
+
+route.delete('/author', (que, ans) => {
+    const id = que.params(id)
+    const answer = Deleting_author(id)
+
+    ans.send(answer)
+    console.log("This will be removing a single author")
+});
 route.listen(2000, () => {
     console.log("\n  This route is working on port 2000\n|-------------------------------------|")
 });
