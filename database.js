4b5e211594b6d86672b66a550ca75973f4ec3782
Mr-Ndi <onlythenotes@gmail.com>
2024-07-13 00:42:00 +0200

    Final changes

--- database.js
+++ database.js
@@ -11,62 +11,245 @@ const pool = mysql.createPool({
 }).promise();
 
 //  ----------------------------- Authors Endpoint ---------------------------------------------
-export async function all_authors() {
+export async function getAllAuthors() {
     try {
-        const [ans] = await pool.query('select first_name, last_name from Authors');
-        return [ans];
+        const [ans] = await pool.query('SELECT author_id, first_name, last_name, date_of_birth FROM Authors');
+        return ans;
     } catch (err) {
         console.error('Error fetching all authors:', err);
         throw err;
     }
 }
 
-export async function Single_author(id) {
+export async function getSingleAuthor(id) {
     try {
         const [ans] = await pool.query('SELECT * FROM Authors WHERE author_id = ?', [id]);
-        return [ans];
+        return ans[0];
     } catch (err) {
         console.error('Error fetching single author:', err);
         throw err;
     }
 }
 
-export async function Create_author(id, fname, lname, dob) {
-    if (!id || !fname || !lname || !dob) {
+export async function createAuthor(id, firstName, lastName, dateOfBirth) {
+    if (!id || !firstName || !lastName || !dateOfBirth) {
         throw new Error('Invalid input parameters');
     }
 
     try {
         const [ans] = await pool.query(
             'INSERT INTO Authors (author_id, first_name, last_name, date_of_birth) VALUES (?, ?, ?, ?)',
-            [id, fname, lname, dob]
+            [id, firstName, lastName, dateOfBirth]
         );
-        return [ans];
+        return ans;
     } catch (err) {
         console.error('Error creating author:', err);
         throw err;
     }
 }
 
-export async function Update_author(id, fname, lname, dob) {
+export async function updateAuthor(id, firstName, lastName, dateOfBirth) {
     try {
         const [ans] = await pool.query(
             'UPDATE Authors SET first_name = ?, last_name = ?, date_of_birth = ? WHERE author_id = ?',
-            [fname, lname, dob, id]
+            [firstName, lastName, dateOfBirth, id]
         );
-        return [ans];
+        return ans;
     } catch (err) {
         console.error('Error updating author:', err);
         throw err;
     }
 }
 
-export async function Deleting_author(id) {
+export async function deleteAuthor(id) {
     try {
         const [ans] = await pool.query('DELETE FROM Authors WHERE author_id = ?', [id]);
-        return [ans];
+        return ans;
     } catch (err) {
         console.error('Error deleting author:', err);
+        throw err;
+    }
+}
+
+//  ----------------------------- Books Endpoint ---------------------------------------------
+export async function getAllBooks() {
+    try {
+        const [ans] = await pool.query('SELECT book_id, title, genre, published_date, author_id FROM Books');
+        return ans;
+    } catch (err) {
+        console.error('Error fetching all books:', err);
+        throw err;
+    }
+}
+
+export async function getSingleBook(id) {
+    try {
+        const [ans] = await pool.query('SELECT * FROM Books WHERE book_id = ?', [id]);
+        return ans[0];
+    } catch (err) {
+        console.error('Error fetching single book:', err);
+        throw err;
+    }
+}
+
+export async function createBook(id, title, genre, publishedDate, authorId) {
+    if (!id || !title || !genre || !publishedDate || !authorId) {
+        throw new Error('Invalid input parameters');
+    }
+
+    try {
+        const [ans] = await pool.query(
+            'INSERT INTO Books (book_id, title, genre, published_date, author_id) VALUES (?, ?, ?, ?, ?)',
+            [id, title, genre, publishedDate, authorId]
+        );
+        return ans;
+    } catch (err) {
+        console.error('Error creating book:', err);
+        throw err;
+    }
+}
+
+export async function updateBook(id, title, genre, publishedDate, authorId) {
+    try {
+        const [ans] = await pool.query(
+            'UPDATE Books SET title = ?, genre = ?, published_date = ?, author_id = ? WHERE book_id = ?',
+            [title, genre, publishedDate, authorId, id]
+        );
+        return ans;
+    } catch (err) {
+        console.error('Error updating book:', err);
+        throw err;
+    }
+}
+
+export async function deleteBook(id) {
+    try {
+        const [ans] = await pool.query('DELETE FROM Books WHERE book_id = ?', [id]);
+        return ans;
+    } catch (err) {
+        console.error('Error deleting book:', err);
+        throw err;
+    }
+}
+
+//  ----------------------------- Members Endpoint ---------------------------------------------
+export async function getAllMembers() {
+    try {
+        const [ans] = await pool.query('SELECT member_id, first_name, last_name, membership_date FROM Members');
+        return ans;
+    } catch (err) {
+        console.error('Error fetching all members:', err);
+        throw err;
+    }
+}
+
+export async function getSingleMember(id) {
+    try {
+        const [ans] = await pool.query('SELECT * FROM Members WHERE member_id = ?', [id]);
+        return ans[0];
+    } catch (err) {
+        console.error('Error fetching single member:', err);
+        throw err;
+    }
+}
+
+export async function createMember(id, firstName, lastName, membershipDate) {
+    if (!id || !firstName || !lastName || !membershipDate) {
+        throw new Error('Invalid input parameters');
+    }
+
+    try {
+        const [ans] = await pool.query(
+            'INSERT INTO Members (member_id, first_name, last_name, membership_date) VALUES (?, ?, ?, ?)',
+            [id, firstName, lastName, membershipDate]
+        );
+        return ans;
+    } catch (err) {
+        console.error('Error creating member:', err);
+        throw err;
+    }
+}
+
+export async function updateMember(id, firstName, lastName, membershipDate) {
+    try {
+        const [ans] = await pool.query(
+            'UPDATE Members SET first_name = ?, last_name = ?, membership_date = ? WHERE member_id = ?',
+            [firstName, lastName, membershipDate, id]
+        );
+        return ans;
+    } catch (err) {
+        console.error('Error updating member:', err);
+        throw err;
+    }
+}
+
+export async function deleteMember(id) {
+    try {
+        const [ans] = await pool.query('DELETE FROM Members WHERE member_id = ?', [id]);
+        return ans;
+    } catch (err) {
+        console.error('Error deleting member:', err);
+        throw err;
+    }
+}
+
+//  ----------------------------- Loans Endpoint ---------------------------------------------
+export async function getAllLoans() {
+    try {
+        const [ans] = await pool.query('SELECT loan_id, book_id, member_id, loan_date, return_date FROM Loans');
+        return ans;
+    } catch (err) {
+        console.error('Error fetching all loans:', err);
+        throw err;
+    }
+}
+
+export async function getSingleLoan(id) {
+    try {
+        const [ans] = await pool.query('SELECT * FROM Loans WHERE loan_id = ?', [id]);
+        return ans[0];
+    } catch (err) {
+        console.error('Error fetching single loan:', err);
+        throw err;
+    }
+}
+
+export async function createLoan(id, bookId, memberId, loanDate, returnDate) {
+    if (!id || !bookId || !memberId || !loanDate || !returnDate) {
+        throw new Error('Invalid input parameters');
+    }
+
+    try {
+        const [ans] = await pool.query(
+            'INSERT INTO Loans (loan_id, book_id, member_id, loan_date, return_date) VALUES (?, ?, ?, ?, ?)',
+            [id, bookId, memberId, loanDate, returnDate]
+        );
+        return ans;
+    } catch (err) {
+        console.error('Error creating loan:', err);
+        throw err;
+    }
+}
+
+export async function updateLoan(id, bookId, memberId, loanDate, returnDate) {
+    try {
+        const [ans] = await pool.query(
+            'UPDATE Loans SET book_id = ?, member_id = ?, loan_date = ?, return_date = ? WHERE loan_id = ?',
+            [bookId, memberId, loanDate, returnDate, id]
+        );
+        return ans;
+    } catch (err) {
+        console.error('Error updating loan:', err);
+        throw err;
+    }
+}
+
+export async function deleteLoan(id) {
+    try {
+        const [ans] = await pool.query('DELETE FROM Loans WHERE loan_id = ?', [id]);
+        return ans;
+    } catch (err) {
+        console.error('Error deleting loan:', err);
         throw err;
     }
 }
--- index.js
+++ index.js
@@ -1,41 +1,227 @@
-import { all_authors, Single_author, Create_author, Deleting_author } from "./database.js";
-import { bodyParse } from "body-parser";
-import express2 from "express"
+import {
+    getAllAuthors, getSingleAuthor, createAuthor, updateAuthor, deleteAuthor,
+    getAllBooks, getSingleBook, createBook, updateBook, deleteBook,
+    getAllMembers, getSingleMember, createMember, updateMember, deleteMember,
+    getAllLoans, getSingleLoan, createLoan, updateLoan, deleteLoan
+} from "./database.js";
+import bodyParser from "body-parser";
+import express from "express";
 
-const route = express2();
-route.use(bodyParse.json())
+const app = express();
+app.use(bodyParser.json());
 
-route.get('/author', async(que, ans) => {
-    const answer = await all_authors();
-    
-    ans.send(answer)
-    console.log("This will be all authors")
+// Authors Endpoints
+app.get('/authors', async (req, res) => {
+    try {
+        const authors = await getAllAuthors();
+        res.status(200).send(authors);
+    } catch (err) {
+        console.error('Error fetching authors:', err);
+        res.status(500).send({ error: 'Error fetching authors' });
+    }
 });
 
-route.get('/author/id', async(que, ans) => {
-    const id = que.params(id)
-    const answer = await Single_author(id)
-
-    ans.send(answer)
-    console.log("This will be single author")
+app.get('/authors/:id', async (req, res) => {
+    try {
+        const author = await getSingleAuthor(req.params.id);
+        res.status(200).send(author);
+    } catch (err) {
+        console.error('Error fetching author:', err);
+        res.status(404).send({ error: 'Author not found' });
+    }
 });
 
-route.put('/author', (que, ans) => {
-    const {id,fname,lname,dob} = que.body;
-    const answer = Create_author(id,fname,lname,dob)
-
-    ans.send(answer)
-    console.log("This will be adding a single author")
+app.post('/authors', async (req, res) => {
+    const { id, firstName, lastName, dateOfBirth } = req.body;
+    try {
+        const result = await createAuthor(id, firstName, lastName, dateOfBirth);
+        res.status(201).send(result);
+    } catch (err) {
+        console.error('Error creating author:', err);
+        res.status(500).send({ error: 'Error creating author' });
+    }
 });
 
-
-route.delete('/author', (que, ans) => {
-    const id = que.params(id)
-    const answer = Deleting_author(id)
-
-    ans.send(answer)
-    console.log("This will be removing a single author")
+app.put('/add/authors/:id', async (req, res) => {
+    const { id, firstName, lastName, dateOfBirth } = req.body;
+    try {
+        const result = await updateAuthor(req.params.id, firstName, lastName, dateOfBirth);
+        res.status(200).send(result);
+    } catch (err) {
+        console.error('Error updating author:', err);
+        res.status(500).send({ error: 'Error updating author' });
+    }
 });
-route.listen(2000, () => {
-    console.log("\n  This route is working on port 2000\n|-------------------------------------|")
+
+app.delete('/remove/authors/:id', async (req, res) => {
+    try {
+        await deleteAuthor(req.params.id);
+        res.status(204).send();
+    } catch (err) {
+        console.error('Error deleting author:', err);
+        res.status(404).send({ error: 'Author not found' });
+    }
+});
+
+// Books Endpoints
+app.get('/books', async (req, res) => {
+    try {
+        const books = await getAllBooks();
+        res.status(200).send(books);
+    } catch (err) {
+        console.error('Error fetching books:', err);
+        res.status(500).send({ error: 'Error fetching books' });
+    }
+});
+
+app.get('/books/:id', async (req, res) => {
+    try {
+        const book = await getSingleBook(req.params.id);
+        res.status(200).send(book);
+    } catch (err) {
+        console.error('Error fetching book:', err);
+        res.status(404).send({ error: 'Book not found' });
+    }
+});
+
+app.post('/books', async (req, res) => {
+    const { id, title, genre, publishedDate, authorId } = req.body;
+    try {
+        const result = await createBook(id, title, genre, publishedDate, authorId);
+        res.status(201).send(result);
+    } catch (err) {
+        console.error('Error creating book:', err);
+        res.status(500).send({ error: 'Error creating book' });
+    }
+});
+
+app.put('/add/books/:id', async (req, res) => {
+    const { id, title, genre, publishedDate, authorId } = req.body;
+    try {
+        const result = await updateBook(req.params.id, title, genre, publishedDate, authorId);
+        res.status(200).send(result);
+    } catch (err) {
+        console.error('Error updating book:', err);
+        res.status(500).send({ error: 'Error updating book' });
+    }
+});
+
+app.delete('/remove/books/:id', async (req, res) => {
+    try {
+        await deleteBook(req.params.id);
+        res.status(204).send();
+    } catch (err) {
+        console.error('Error deleting book:', err);
+        res.status(404).send({ error: 'Book not found' });
+    }
+});
+
+// Members Endpoints
+app.get('/members', async (req, res) => {
+    try {
+        const members = await getAllMembers();
+        res.status(200).send(members);
+    } catch (err) {
+        console.error('Error fetching members:', err);
+        res.status(500).send({ error: 'Error fetching members' });
+    }
+});
+
+app.get('/members/:id', async (req, res) => {
+    try {
+        const member = await getSingleMember(req.params.id);
+        res.status(200).send(member);
+    } catch (err) {
+        console.error('Error fetching member:', err);
+        res.status(404).send({ error: 'Member not found' });
+    }
+});
+
+app.post('/members', async (req, res) => {
+    const { id, firstName, lastName, membershipDate } = req.body;
+    try {
+        const result = await createMember(id, firstName, lastName, membershipDate);
+        res.status(201).send(result);
+    } catch (err) {
+        console.error('Error creating member:', err);
+        res.status(500).send({ error: 'Error creating member' });
+    }
+});
+
+app.put('/add/members/:id', async (req, res) => {
+    const { id, firstName, lastName, membershipDate } = req.body;
+    try {
+        const result = await updateMember(req.params.id, firstName, lastName, membershipDate);
+        res.status(200).send(result);
+    } catch (err) {
+        console.error('Error updating member:', err);
+        res.status(500).send({ error: 'Error updating member' });
+    }
+});
+
+app.delete('/remove/members/:id', async (req, res) => {
+    try {
+        await deleteMember(req.params.id);
+        res.status(204).send();
+    } catch (err) {
+        console.error('Error deleting member:', err);
+        res.status(404).send({ error: 'Member not found' });
+    }
+});
+
+// Loans Endpoints
+app.get('/loans', async (req, res) => {
+    try {
+        const loans = await getAllLoans();
+        res.status(200).send(loans);
+    } catch (err) {
+        console.error('Error fetching loans:', err);
+        res.status(500).send({ error: 'Error fetching loans' });
+    }
+});
+
+app.get('/loans/:id', async (req, res) => {
+    try {
+        const loan = await getSingleLoan(req.params.id);
+        res.status(200).send(loan);
+    } catch (err) {
+        console.error('Error fetching loan:', err);
+        res.status(404).send({ error: 'Loan not found' });
+    }
+});
+
+app.post('/loans', async (req, res) => {
+    const { id, bookId, memberId, loanDate, returnDate } = req.body;
+    try {
+        const result = await createLoan(id, bookId, memberId, loanDate, returnDate);
+        res.status(201).send(result);
+    } catch (err) {
+        console.error('Error creating loan:', err);
+        res.status(500).send({ error: 'Error creating loan' });
+    }
+});
+
+app.put('/add/loans/:id', async (req, res) => {
+    const { id, bookId, memberId, loanDate, returnDate } = req.body;
+    try {
+        const result = await updateLoan(req.params.id, bookId, memberId, loanDate, returnDate);
+        res.status(200).send(result);
+    } catch (err) {
+        console.error('Error updating loan:', err);
+        res.status(500).send({ error: 'Error updating loan' });
+    }
+});
+
+app.delete('/remove/loans/:id', async (req, res) => {
+    try {
+        await deleteLoan(req.params.id);
+        res.status(204).send();
+    } catch (err) {
+        console.error('Error deleting loan:', err);
+        res.status(404).send({ error: 'Loan not found' });
+    }
+});
+
+app.listen(2000, () => {
+    console.log('\n  This route is working on port 2000\n|-------------------------------------|');
 });
